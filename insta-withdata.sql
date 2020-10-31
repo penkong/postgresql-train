e0df0a5a3b839fbdf0b7322ee087b0790332551c
mazdak <mazdak@localhost.localdomain>
2020-10-31 18:10:28 +0100

    more on

---
+++ insta-withdata.sql
@@ -0,0 +1,20 @@
+
+--select id from users order by id desc limit 3
+--select username ,caption from posts join users on users.id = posts.user_id where posts.user_id = 200;
+
+--select
+--	username,
+--	count(*)
+--from
+--	likes
+--join users on
+--	users.id = likes.user_id
+--group by
+--	users.username; 
+
+
+--show data_directory;
+
+--select  oid, datname from pg_database;
+
+--select * from pg_class;
