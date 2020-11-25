758e1c269b234625dbd1592c0c52eeee46026f0e
mazdak <nazemi.works@gmail.com>
2020-11-25 11:53:55 +0000

    review

--- with-test/src/test/context.js
+++ with-test/src/test/context.js
@@ -1,37 +1,37 @@
-const { randomBytes } = require('crypto');
-const format = require('pg-format');
-const { default: migrate } = require('node-pg-migrate');
-const pool = require('../pool');
+const { randomBytes } = require("crypto");
+const { default: migrate } = require("node-pg-migrate");
+const format = require("pg-format");
+
+const pool = require("../pool");
 
 const DEFAULT_OPTS = {
-  host: 'localhost',
+  host: "localhost",
   port: 5432,
-  database: 'socialnet-test',
-  user: 'postgres',
-  password: '',
+  database: "socialnet-test",
+  user: "postgres",
+  password: "",
 };
 
 class Context {
-
   constructor(roleName) {
     this.roleName = roleName;
   }
 
   static async build() {
     // Randomly generating a role name to connect to PG as
-    const roleName = 'a' + randomBytes(4).toString('hex');
+    const roleName = "a" + randomBytes(4).toString("hex");
 
     // Connect to PG as usual
     await pool.connect(DEFAULT_OPTS);
 
     // Create a new role
     await pool.query(
-      format('CREATE ROLE %I WITH LOGIN PASSWORD %L;', roleName, roleName)
+      format("CREATE ROLE %I WITH LOGIN PASSWORD %L;", roleName, roleName)
     );
 
     // Create a schema with the same name
     await pool.query(
-      format('CREATE SCHEMA %I AUTHORIZATION %I;', roleName, roleName)
+      format("CREATE SCHEMA %I AUTHORIZATION %I;", roleName, roleName)
     );
 
     // Disconnect entirely from PG
@@ -40,14 +40,14 @@ class Context {
     // Run our migrations in the new schema
     await migrate({
       schema: roleName,
-      direction: 'up',
+      direction: "up",
       log: () => {},
       noLock: true,
-      dir: 'migrations',
+      dir: "migrations",
       databaseUrl: {
-        host: 'localhost',
+        host: "localhost",
         port: 5432,
-        database: 'socialnetwork-test',
+        database: "socialnetwork-test",
         user: roleName,
         password: roleName,
       },
@@ -55,17 +55,15 @@ class Context {
 
     // Connect to PG as the newly created role
     await pool.connect({
-      host: 'localhost',
+      host: "localhost",
       port: 5432,
-      database: 'socialnetwork-test',
+      database: "socialnetwork-test",
       user: roleName,
       password: roleName,
     });
 
     return new Context(roleName);
   }
-
-  
 
   async reset() {
     return pool.query(`
@@ -81,8 +79,8 @@ class Context {
     await pool.connect(DEFAULT_OPTS);
 
     // Delete the role and schema we created
-    await pool.query(format('DROP SCHEMA %I CASCADE;', this.roleName));
-    await pool.query(format('DROP ROLE %I;', this.roleName));
+    await pool.query(format("DROP SCHEMA %I CASCADE;", this.roleName));
+    await pool.query(format("DROP ROLE %I;", this.roleName));
 
     // Disconnect
     await pool.close();
